303ce3319460e53a87c0a3192e3c0db305382cad
kasheen8 <abinaevravil@mail.ru>
2020-08-18 17:47:21 +0400

    Завершена 1 стадия проекта. Созданы модели "Вакансии" и "Резюме"

--- HyperJob Agency/task/hyperjob/vacancy/models.py
+++ HyperJob Agency/task/hyperjob/vacancy/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Vacancy(models.Model):
+    description = models.CharField(max_length=1024)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='author_of_vacancy', on_delete=models.CASCADE)
+
+    class Meta:
+        app_label = 'vacancy'
